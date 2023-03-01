00f8d1efeb10acf540b4c31a4cc049fdc9abb44b
EFE ESEN (Elfesya) <web.elfesyaesen@gmail.com>
2023-03-01 11:00:55 +0300

    php routing

---
+++ config/routing.php
@@ -0,0 +1,28 @@
+<?php 
+class Routing {
+    public static function route()
+    { 
+        $URL = explode('&', ltrim(str_replace([basename($_SERVER['SCRIPT_NAME']), dirname($_SERVER['SCRIPT_NAME'])], '',$_SERVER['REQUEST_URI']),'/'));
+        if ($URL[0] == '') {
+            $controller = 'IndexController';
+            $method     = 'index';
+            call_user_func_array([new $controller, $method], []);
+        } else {
+            $controller = ucfirst($URL[0]) . 'Controller';
+            $method = 'index';
+            if(isset($URL[1])) {
+                $method = strtolower($URL[1]);
+            }
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                if(isset($_FILES)) {
+                    $args = [$_POST, $_FILES];
+                } else { $args = [$_POST]; }
+                call_user_func_array([new $controller, $method], $args);
+            } elseif ($_SERVER['REQUEST_METHOD'] == 'GET'){
+                $args = [$_GET];
+                call_user_func_array([new $controller, $method], $args);
+            }
+        }
+    }
+}
---
+++ controller/IndexController.php
@@ -0,0 +1,7 @@
+<?php
+class IndexController extends Controller {
+	public function index(){
+		require_once 'view/index.php';
+	}
+
+}
