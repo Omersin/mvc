71285148fc6041a6f89b26da226514441e7eb862
EFE ESEN (Elfesya) <web.elfesyaesen@gmail.com>
2023-03-01 11:37:06 +0300

    view fonksiyonunun tanımlanması...

--- config/controller.php
+++ config/controller.php
@@ -1,4 +1,7 @@
 <?php 
 class Controller {
+    public function view($filename){
+		require_once 'view/'.$filename.'.php';
+	}
 	
 }
--- controller/IndexController.php
+++ controller/IndexController.php
@@ -1,7 +1,7 @@
 <?php
 class IndexController extends Controller {
 	public function index(){
-		require_once 'view/index.php';
+		$this->view('index');
 	}
 
 }
