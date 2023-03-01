094a242eadca9d5d8cca2774f6928884ec748522
EFE ESEN (Elfesya) <web.elfesyaesen@gmail.com>
2023-03-01 11:43:05 +0300

    update

--- config/controller.php
+++ config/controller.php
@@ -1,13 +1,7 @@
 <?php 
 class Controller {
-<<<<<<< HEAD
     public function view($filename, $data = []){
 		require_once 'view/'.$filename.'.php';
 	}
-=======
-    public function view($filename){
-	require_once 'view/'.$filename.'.php';
-    }
->>>>>>> 01d7c01f55400fe85983b79e8812bbfc8377dc9e
-	
+
 }
